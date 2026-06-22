487ccfaa3a051c5533b4206ca0950a9897d16e2f
Alessio Barducci <alessio.barducci@team.blue>
2026-06-22 14:39:32 +0200

    improved collision logic and added CPU movement strategy

--- src/game/systems/CollisionSystem.ts
+++ src/game/systems/CollisionSystem.ts
@@ -1,8 +1,10 @@
-import { BorderLimits } from "../geometry/BorderLimits";
 import { GameConfigs } from "../../utils/GameConfigs";
-import { GameWorld } from "../world/GameWorld";
-import { MovementPoint } from "../geometry/MovementPoint";
+import { Player } from "../entities/Player";
 import { BallStatus } from "../enums/BallStatus";
+import { BorderLimits } from "../geometry/BorderLimits";
+import { MovementPoint } from "../geometry/MovementPoint";
+import { Point } from "../geometry/Point";
+import { GameWorld } from "../world/GameWorld";
 
 export class CollisionSystem {
     public constructor(private gameConfigs: GameConfigs) {}
@@ -11,6 +13,7 @@ export class CollisionSystem {
         this.checkBallBorderCollisions(gameWorld);
         this.checkPlayerBorderCollisions(gameWorld);
         this.checkBallPlayerCollision(gameWorld);
+        this.checkPlayerCollisions(gameWorld);
     }
 
     private checkBallBorderCollisions(gameWorld: GameWorld): void {
@@ -99,5 +102,53 @@ export class CollisionSystem {
                     }
                 });
         }
+    }
+
+    private checkPlayerCollisions(gameWorld: GameWorld): void {
+        const humanPlayer = gameWorld.players.find(player => !player.isSubstitute && !player.isCpu);
+        const cpuPlayer = gameWorld.players.find(player => !player.isSubstitute && player.isCpu);
+
+        if (humanPlayer === undefined || cpuPlayer === undefined) {
+            return;
+        }
+
+        if (MovementPoint.areTouching(humanPlayer.movementPosition, cpuPlayer.movementPosition)) {
+            const intersectionPoint = new Point(
+                (humanPlayer.movementPosition.position.x + cpuPlayer.movementPosition.position.x) /
+                    2,
+                (humanPlayer.movementPosition.position.y + cpuPlayer.movementPosition.position.y) /
+                    2,
+            );
+            const collisionSpeed =
+                (humanPlayer.movementPosition.getSpeed() + cpuPlayer.movementPosition.getSpeed()) /
+                2;
+            this.bouncePlayers(humanPlayer, cpuPlayer, intersectionPoint, collisionSpeed);
+            this.bouncePlayers(cpuPlayer, humanPlayer, intersectionPoint, collisionSpeed);
+
+            const ball = gameWorld.ball;
+            if (ball.ballStatus === BallStatus.ATTACHED) {
+                ball.movementPosition.setSpeed(
+                    collisionSpeed,
+                    Point.getAngleBetweenPoints(intersectionPoint, ball.movementPosition.position),
+                );
+                ball.ballStatus = BallStatus.FREE;
+            }
+        }
+    }
+
+    private bouncePlayers(
+        player1: Player,
+        player2: Player,
+        intersectionPoint: Point,
+        collisionSpeed: number,
+    ): void {
+        const angle =
+            Point.getAngleBetweenPoints(player1.movementPosition.position, intersectionPoint) -
+            Math.PI;
+        player1.movementPosition.setSpeed(collisionSpeed, angle);
+        player1.movementPosition.position.x =
+            intersectionPoint.x + Math.cos(angle) * player2.movementPosition.size;
+        player1.movementPosition.position.y =
+            intersectionPoint.y + Math.sin(angle) * player2.movementPosition.size;
     }
 }
---
+++ src/game/systems/playersMovementStrategies/CpuMovementStrategy.ts
@@ -0,0 +1,39 @@
+import { Player } from "../../entities/Player";
+import { BallStatus } from "../../enums/BallStatus";
+import { GameStatus } from "../../enums/GameStatus";
+import { GameWorld } from "../../world/GameWorld";
+import { AbstractPlayerMovementStrategy } from "./AbstractPlayerMovementStrategy";
+
+export class CpuMovementStrategy extends AbstractPlayerMovementStrategy {
+    public canBeApplied(player: Player, gameWorld: GameWorld): boolean {
+        return (
+            !player.isSubstitute &&
+            player.isCpu &&
+            gameWorld.gameStatusManager.gameStatus === GameStatus.PLAYING
+        );
+    }
+
+    public apply(player: Player, gameWorld: GameWorld, deltaMs: number): void {
+        // TODO just random draft of CPU logic
+        const ball = gameWorld.ball;
+        player.currentMaxSpeed = player.normalMaxSpeed / 2;
+        if (ball.ballStatus === BallStatus.FREE) {
+            player.destinationPosition.position = ball.movementPosition.position;
+            player.adjustSpeedToDestinationPoint(deltaMs);
+        } else if (ball.ballStatus === BallStatus.ATTACHED) {
+            const attachedPlayer = ball.attachedPlayer;
+            if (attachedPlayer === null || attachedPlayer.isCpu) {
+                const delta = player.movementPosition.acceleration * deltaMs;
+                player.movementPosition.velocity.x =
+                    Math.sign(player.movementPosition.velocity.x) *
+                    Math.max(Math.abs(player.movementPosition.velocity.x) - delta, 0);
+                player.movementPosition.velocity.y =
+                    Math.sign(player.movementPosition.velocity.y) *
+                    Math.max(Math.abs(player.movementPosition.velocity.y) - delta, 0);
+            } else {
+                player.destinationPosition.position = attachedPlayer.movementPosition.position;
+                player.adjustSpeedToDestinationPoint(deltaMs);
+            }
+        }
+    }
+}

15d2341b9bd0f178bc94cf39324262eac207e98e
alebar12 <>
2026-07-08 14:02:00 +0200

    chore: added configuration for typescript check during development

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
   base: process.env['NODE_ENV'] === 'production'
     ? '/insane-soccer/'
     : '/',
+  plugins: [
+    checker({
+      typescript: true,
+      eslint: { lintCommand: 'eslint src --ext .ts' },
+    }),
+  ],
 })

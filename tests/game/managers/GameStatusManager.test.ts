dcfa20a3af26f48caeb5654483977fd8882532e7
alebar12 <abarducci@proton.me>
2026-08-20 15:37:14 +0200

    feat: added game status manager tests

---
+++ tests/game/managers/GameStatusManager.test.ts
@@ -0,0 +1,43 @@
+import { GameStatus } from "@/game/enums/GameStatus";
+import { GameStatusManager } from "@/game/managers/GameStatusManager";
+import { EventBus } from "ts-bus";
+import { beforeEach, describe, expect, it, vi } from "vitest";
+
+describe("GameStatusManager", () => {
+    let bus: Pick<EventBus, "publish">;
+    let gameStatusManager: GameStatusManager;
+
+    beforeEach(() => {
+        bus = {
+            publish: vi.fn(),
+        };
+        gameStatusManager = new GameStatusManager(bus as EventBus);
+    });
+
+    describe("init", () => {
+        it("should start with MENU status", () => {
+            expect(gameStatusManager.gameStatus).toBe(GameStatus.MENU);
+        });
+    });
+
+    describe("changeStatus", () => {
+        it("should change status", () => {
+            gameStatusManager.update(5000);
+            expect(gameStatusManager.isStatusChangedRecently()).toBe(false);
+            gameStatusManager.changeStatus(GameStatus.WAITING_BALL);
+            expect(gameStatusManager.gameStatus).toBe(GameStatus.WAITING_BALL);
+            expect(gameStatusManager.isStatusChangedRecently()).toBe(true);
+        });
+    });
+
+    describe("schedule status change", () => {
+        it("should schedule status change", () => {
+            gameStatusManager.scheduleStatusChange(1000, GameStatus.WAITING_BALL);
+            expect(gameStatusManager.gameStatus).toBe(GameStatus.MENU);
+            gameStatusManager.update(500);
+            expect(gameStatusManager.gameStatus).toBe(GameStatus.MENU);
+            gameStatusManager.update(500);
+            expect(gameStatusManager.gameStatus).toBe(GameStatus.WAITING_BALL);
+        });
+    });
+});
